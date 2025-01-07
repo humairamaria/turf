a26388e29e4edf4d38710a57a769f729b8eedcce
humairamaria <humaira.binte.moshiur@g.bracu.ac.bd>
2025-01-08 04:24:18 +0600

    Update turf_history.php

--- turf_history.php
+++ turf_history.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-// Ensure the user is logged in
+
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
     exit;
 }
 
-// Connect to the database
+
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -15,12 +15,11 @@ $dbname = "turf";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-// Fetch bookings for the logged-in user
+
 $user_id = $_SESSION['user_id'];
 $sql = "
     SELECT 
