6afd59674ea9cb44140a5341ae4e5426f703fe64
humairamaria <humaira.binte.moshiur@g.bracu.ac.bd>
2025-01-08 04:14:53 +0600

    Update connect.php

--- connect.php
+++ connect.php
@@ -5,28 +5,14 @@ $USERNAME = 'root';
 $PASSWORD = '';
 $DATABASE = 'turf';
 
-// Create connection
+
 $conn = new mysqli($servername, $USERNAME, $PASSWORD, $DATABASE);
 
-// Check connection
+
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
 
-//$servername= 'localhost';
-//$USERNAME= 'root';
-//$PASSWORD='';
-//$DATABaSE='turf';
-
-
-//$conn=new mysqli($servername,$USERNAME,$PASSWORD,$DATABaSE);
-//if ($conn->connect_error){
-    //echo "connection unsuccesful" .$conn->connect_error;
-
-//}else{
-    //echo 'connected';
-//}
-//$conn->close();
 
 ?>
